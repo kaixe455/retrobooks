34badeb1cdf0105fd2a97e5f2c7a71337dda6508
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2023-08-31 15:20:06 +0200

    Config RetroBooks font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				retro: {
+					DEFAULT: '#7541e4'
+				}
+			},
+			fontFamily: {
+				krona: ['KronaOne', 'system-ui', 'sans-serif']
+			},
+			backgroundImage: {
+				'retro-bg': 'linear-gradient(90deg, #0F262B 100%, 62.9447%, #090C25 100%)'
+			}
+		}
 	},
+	safelist: [
+		'retro-bg'
+	],
 	plugins: [],
 }
